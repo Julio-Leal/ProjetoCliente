83ed50f7597dfa7b67878dade17815c08f36b043
Julio-Leal <cavalcantejulio522@gmail.com>
2026-04-27 23:11:34 -0300

    Correções importantes: classe funcionando

--- src/main/java/br/com/cliente/ClienteMain.java
+++ src/main/java/br/com/cliente/ClienteMain.java
@@ -9,103 +9,140 @@ import com.google.gson.Gson;
 import br.com.model.Mensagem;
 
 public class ClienteMain {
-	private static Scanner input = new Scanner(System.in);
-	private static String ip;
-	private static int porta;
-	
-	public static void main(String[] args) {		
-		try {
-			informarIPEPorta();
-			
-			Socket socket = new Socket(ip, porta);
-			
-			System.out.println("[servidor]: Conectado!");
-			
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			Scanner input = new Scanner(System.in);
-			Gson gson = new Gson();
-			
-			//thread PARA RECEER MENSAGENS
-			ClienteReceiver receiver = new ClienteReceiver(socket);
-			new Thread(receiver).start();
-			
-            // =========================
-            // MENU INICIAL
-            // =========================
-			System.out.println("1 - Login");
-			System.out.println("2 - Cadastro");
-			
-			String opcao = input.nextLine();
-			
-			if(opcao.equals("1")) {
-				Mensagem login = new Mensagem();
-				login.setOp("login");
-				
-				System.out.println("Usuário: ");
-				login.setUsuario(input.nextLine());
-				System.out.println("Senha: ");
-				login.setSenha(input.nextLine());
-				
-				out.println(gson.toJson(login));
-			} else if(opcao.equals("2")) {
-				Mensagem cadastro = new Mensagem();
-				cadastro.setOp("cadastro");
-				
-				System.out.println("Nome: ");
-				cadastro.setNome(input.nextLine());
-				System.out.println("Usuário: ");
-				cadastro.setUsuario(input.nextLine());
-				System.out.println("Senha: ");
-				cadastro.setSenha(input.nextLine());
-				
-				out.println(gson.toJson(cadastro));
-				
-				System.out.println("Agora faça login...");
-				return;
-			}
-			
-            // =========================
-            // CHAT
-            // =========================
-			String token = null;
-			
-			System.out.println("Digite seu token (retornado pelo servidor):");
-			token = input.nextLine();
-			
-			while(true) {
-				
-				System.out.println("Destinatário: ");
-				String destinatario = input.nextLine();
-				
-				System.out.println("Mensagem: ");
-				String texto = input.nextLine();
-				
-				if(texto.equalsIgnoreCase("/sair")) {
-					break;
-				}
-				
-				Mensagem msg = new Mensagem();
-				msg.setOp("enviarMensagem");
-				msg.setToken(token);
-				msg.setDestinatario(destinatario);
-				msg.setMensagem(texto);
-				
-				out.println(gson.toJson(msg));
-			
-			}
-			
-		} catch(Exception e) {
-			System.out.println("❌ - Erro ao conectar: ");
-			e.printStackTrace();
-		}
-	
-	}
-	
-	private static void informarIPEPorta() {
-		System.out.println("[]: Informe o IP do servidor: ");
-		ip = input.nextLine();
-			
-		System.out.println("[]: Informe a Porta do servidor: ");
-		porta = input.nextInt();
-	}
+
+    private static Scanner input = new Scanner(System.in);
+    private static String ip;
+    private static int porta;
+
+    public static String token;
+
+    public static void main(String[] args) {
+
+        try {
+            informarIPEPorta();
+
+            Socket socket = new Socket(ip, porta);
+
+            
+            System.out.println("[servidor]: Conectado!");
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Gson gson = new Gson();
+
+            ClienteReceiver receiver = new ClienteReceiver(socket);
+            new Thread(receiver).start();
+
+            while (true) {
+
+                System.out.println("\n========== MENU ==========");
+                System.out.println("1 - Login");
+                System.out.println("2 - Cadastrar");
+                System.out.println("3 - Consultar");
+                System.out.println("4 - Atualizar");
+                System.out.println("5 - Deletar");
+                System.out.println("6 - Logout");
+                System.out.println("7 - Enviar mensagem\n");
+
+                String opcao = input.nextLine();
+
+                Mensagem msg = new Mensagem();
+
+                switch (opcao) {
+
+                    case "1":
+                        msg.setOp("login");
+
+                        System.out.print("Usuário: ");
+                        msg.setUsuario(input.nextLine());
+
+                        System.out.print("Senha: ");
+                        msg.setSenha(input.nextLine());
+
+                        out.println(gson.toJson(msg));
+                        break;
+
+                    case "2":
+                        msg.setOp("cadastrarUsuario");
+
+                        System.out.print("Nome: ");
+                        msg.setNome(input.nextLine());
+
+                        System.out.print("Usuário: ");
+                        msg.setUsuario(input.nextLine());
+
+                        System.out.print("Senha: ");
+                        msg.setSenha(input.nextLine());
+
+                        out.println(gson.toJson(msg));
+                        break;
+
+                    case "3":
+                        msg.setOp("consultarUsuario");
+                        msg.setToken(token);
+                        out.println(gson.toJson(msg));
+                        break;
+
+                    case "4":
+                        msg.setOp("atualizarUsuario");
+                        msg.setToken(token);
+
+                        System.out.print("Novo nome: ");
+                        msg.setNovoNome(input.nextLine());
+                        
+                        msg.setNovoUsuario(input.nextLine());
+                        
+                        System.out.print("Nova senha: ");
+                        msg.setNovaSenha(input.nextLine());
+
+                        out.println(gson.toJson(msg));
+                        break;
+
+                    case "5":
+                        msg.setOp("deletarUsuario");
+                        msg.setToken(token);
+                        out.println(gson.toJson(msg));
+                        break;
+
+                    case "6":
+                        msg.setOp("logout");
+                        msg.setToken(token);
+                        out.println(gson.toJson(msg));
+                        token = null;
+                        break;
+
+                    case "7":
+
+                        System.out.print("Destinatário: ");
+                        String dest = input.nextLine();
+
+                        System.out.print("Mensagem: ");
+                        String texto = input.nextLine();
+
+                        msg.setOp("enviarMensagem");
+                        msg.setToken(token);
+                        msg.setDestinatario(dest);
+                        msg.setMensagem(texto);
+
+                        out.println(gson.toJson(msg));
+                        break;
+                   default:
+                	   System.out.println("Opção inválida!");
+                	   break;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar");
+            e.printStackTrace();
+        }
+    }
+
+    private static void informarIPEPorta() {
+
+        System.out.print("IP: ");
+        ip = input.nextLine();
+
+        System.out.print("Porta: ");
+        porta = Integer.parseInt(input.nextLine());
+    }
 }
--- src/main/java/br/com/cliente/ClienteReceiver.java
+++ src/main/java/br/com/cliente/ClienteReceiver.java
@@ -4,29 +4,43 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class ClienteReceiver implements Runnable{
-	private Socket socket;
+import com.google.gson.Gson;
 
-	public ClienteReceiver(Socket socket) {
-		this.socket = socket;
-	}
-	
-	@Override
-	public void run() {
-		try {
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(socket.getInputStream())
-			);
-			
-			String mensagem;
-			
-			while((mensagem = in.readLine()) != null) {
-				System.out.println(mensagem);
-			}
-			
-		} catch (Exception e) {
-			System.out.println("Conexão encerrada.");
-			e.printStackTrace();
-		}
-	}
+import br.com.model.Mensagem;
+
+public class ClienteReceiver implements Runnable {
+
+    private Socket socket;
+
+    public ClienteReceiver(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            Gson gson = new Gson();
+
+            String json;
+
+            while ((json = in.readLine()) != null) {
+
+                Mensagem msg = gson.fromJson(json, Mensagem.class);
+
+                System.out.println("Servidor: " + json);
+
+                if (msg.getToken() != null) {
+                    ClienteMain.token = msg.getToken();
+                    System.out.println("TOKEN RECEBIDO: " + msg.getToken());
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Conexão encerrada.");
+        }
+    }
 }
