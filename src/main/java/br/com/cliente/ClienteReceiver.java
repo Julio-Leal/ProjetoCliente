a423c23985ee96aefc4c1f7501df90ea8edcee95
Julio-Leal <cavalcantejulio522@gmail.com>
2026-05-17 19:23:43 -0300

    Update> Atualiza a classe ClienteReceiver.java

--- src/main/java/br/com/cliente/ClienteReceiver.java
+++ src/main/java/br/com/cliente/ClienteReceiver.java
@@ -11,34 +11,27 @@ import br.com.model.Mensagem;
 public class ClienteReceiver implements Runnable {
 
     private Socket socket;
-
+    
     public ClienteReceiver(Socket socket) {
         this.socket = socket;
     }
 
     @Override
     public void run() {
-
         try {
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Gson gson = new Gson();
 
             String json;
 
             while ((json = in.readLine()) != null) {
-
                 Mensagem msg = gson.fromJson(json, Mensagem.class);
-
-                System.out.println("Servidor: " + json);
-
-                if (msg.getToken() != null) {
-                    ClienteMain.token = msg.getToken();
-                    System.out.println("TOKEN RECEBIDO: " + msg.getToken());
+                System.out.println("[Servidor]: " + json);
+                
+                if(msg.getResposta().equals("200") && msg.getToken() != null) {
+                	ClienteMain.token = msg.getToken();
                 }
             }
-
         } catch (Exception e) {
             System.out.println("Conexão encerrada.");
         }
